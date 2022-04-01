30eebbbd2e4886a6734a435a69eedf9379d8e0ba
Марина Молчанова <notmarina@bk.ru>
2022-04-01 17:11:43 +0300

    lab1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo.png" alt="" height="90px">
+        <div class="header__title">
+            <h1>Самостоятельная работа</h1>
+        </div>
+        <h2>Молчанова Марина211-323</h2>
+    </header>
+    <main>
+        <?php
+        echo '<p>Hello world!</p>';
+        ?>
+    </main>
+    <footer>
+        Создать веб-страницу с динамическим контентом и загрузить код в удалённый репозиторий.
+    </footer>
+
+</body>
+</html>
